2c752d0abc831abf15b3c1687f9dcc52dc8e07a9
johny babylon <ztredded@gmail.com>
2015-04-02 11:21:52 -0700

    add image_path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,21 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'zhomos.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 587,
+      domain:               'sendgrid.com',
+      user_name:            'ztredded',
+      password:             'gatesjobs1',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
