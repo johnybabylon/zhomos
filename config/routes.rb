4201f6acb861befd09e920015c81c86283edd55f
johny babylon <ztredded@gmail.com>
2015-04-02 12:22:13 -0700

    add griddler

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'inmails/index'
 
+  post '/email_processor' => 'griddler/emails#create', as: :email_processor
+
+
   resources :outmails
 
   # The priority is based upon order of creation: first created -> highest priority.
