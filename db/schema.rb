e29a3e0b83b8ae789d95faa80b495c90a29ffed4
johny babylon <ztredded@gmail.com>
2015-04-02 12:16:25 -0700

    add griddler

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401224036) do
+ActiveRecord::Schema.define(version: 20150402185946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "inmails", force: :cascade do |t|
+    t.text     "body"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "outmails", force: :cascade do |t|
     t.string   "to"
