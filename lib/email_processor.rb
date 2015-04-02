f477e65707f13308a7f19c1f12f3cf31608b76ca
johny babylon <ztredded@gmail.com>
2015-04-02 13:17:51 -0700

    add griddler

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,5 +1,20 @@
 class EmailProcessor
-  def self.process(email)
-    Inmail.create!({ body: email.body, email: email.from })
+
+  def initialize(email)
+    @email = email
   end
+
+  def process
+    # all of your application-specific code here - creating models,
+    # processing reports, etc
+
+    # Here's an example of model creation
+    Inmail.create!(
+        subject: @email.subject,
+        body: @email.body
+    )
+  end
+
+
+
 end
