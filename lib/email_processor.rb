49c8470b2387315658388bba1bbbc8a4dce1a228
johny babylon <ztredded@gmail.com>
2015-04-03 16:31:18 -0700

    add griddler

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,6 +1,7 @@
 class EmailProcessor
     def self.process(email)
-      Inmail.create!({ body: email.body, from: email.from })
+      Inmail.create({ body: email.body, email: email.from })
     end
   end
+
 
