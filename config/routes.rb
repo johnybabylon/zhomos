c11dae22758f6c39944860f10b1a99da2e77589c
johny babylon <ztredded@gmail.com>
2015-04-08 22:42:20 -0700

    change front end

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   #mount_griddler
 
   resources :outmails
-
+  resources :inmails
 
 
   #get "/email_processor", to: proc { [200, {}, ["OK"]] }, as: "mandrill_head_test_request"
