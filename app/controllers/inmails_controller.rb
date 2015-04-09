e16642ce84b85b25e8bb6101c7d4b977b56cd763
johny babylon <ztredded@gmail.com>
2015-04-08 23:59:14 -0700

    front end

--- app/controllers/inmails_controller.rb
+++ app/controllers/inmails_controller.rb
@@ -4,7 +4,27 @@ class InmailsController < ApplicationController
   end
 
   def destroy
+    @inmail.destroy
+    respond_to do |format|
+      format.html { redirect_to outmails_url, notice: 'Outmail was successfully destroyed.' }
+      format.json { head :no_content }
+    end
 
   end
 
+  def show
+
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_outmail
+    @outmail = Outmail.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def outmail_params
+    params.require(:outmail).permit(:to, :subject, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'inmails/index'
+
 
   post '/email_processor' => 'griddler/emails#create', as: :email_processor
 
