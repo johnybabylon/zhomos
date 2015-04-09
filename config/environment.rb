4a4cc632ccf791f49dcebc7f2c8df6db64e06ce0
johny babylon <ztredded@gmail.com>
2015-04-08 21:50:59 -0700

    hide info

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+
+# Load the app's custom environment variables here, before environments/*.rb
+email_password = File.join(Rails.root, 'config', 'initializers', 'email_password.rb')
+load(email_password) if File.exists?(email_password)
+
+
 # Initialize the Rails application.
 Rails.application.initialize!
