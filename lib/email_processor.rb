3d418adcd0f816bf56f130db096afd927d4752ca
johny babylon <ztredded@gmail.com>
2015-04-23 02:42:52 -0700

    inmail

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -23,7 +23,7 @@ class EmailProcessor
       #user = User.find_by zmail: @email.to
       #if user.zmail == @email.to
       Inmail.create(from: email.from, body: email.body)
-        end
+
 
     end
 
