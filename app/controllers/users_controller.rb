038bbf077bdb3d6e3b7967a92c0e9588edc1115d
johny babylon <ztredded@gmail.com>
2015-04-23 03:42:51 -0700

    inmail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,10 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_user
+        @user = User.find(params[:id])
 
+    end
 
 
 
